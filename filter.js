9cae8480f561ee4e3d987a2fefc9c7f4f3c0892b
Benmissi-A <aminebenmissi@hotmail.fr>
2021-03-04 13:52:58 +0100

    all finished clean

--- filter.js
+++ filter.js
@@ -1,12 +1,10 @@
 const filter = (array,compareValue) => {
-  let tmpArray=[...array]
   for(let i=0; i < array.length;i++){
-    if(tmpArray[i]===compareValue){  
+    if(array[i]===compareValue){  
       return true
     }else{
       return false
     }
   }
-
 }
 console.log(filter([1,2,3],9))

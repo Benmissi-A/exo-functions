5e01497e1b6a0523ebebb7905532e5fdd3c89806
Benmissi-A <aminebenmissi@hotmail.fr>
2021-03-05 11:50:07 +0100

    makeUnique fixed

--- arrayUtils.js
+++ arrayUtils.js
@@ -43,3 +43,27 @@ const sortAscend = (array) => {
 } 
 
 console.log(sortAscend([99, 100, 101, 1]))
+
+
+const makeUnique = (array) => {
+  let tab = [...array]
+  let x=0
+  let tab2 = []
+  
+  for(let j=0 ; j < array.length ; j++){
+    let nb=tab[0]
+    for(let i = 0 ; i < array.length ; i++ ){
+      if(tab[i]>=nb && !tab2.includes(nb)){
+        nb = tab[i]
+        x = i
+      }else if(tab[i]===0){
+        nb=0
+      }
+    }
+    tab.splice(x,1)
+    tab2.push(nb)
+  }
+  return tab2.reverse('')
+} 
+
+console.log(makeUnique([99, 100,100, 101,100, 1]))
